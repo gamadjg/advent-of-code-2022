b235d52889d9802a645994a6542eaa22d3c7e82c
gamadjg <gamadavid36@gmail.com>
2022-12-10 16:01:24 -0800

    day 1 complete

---
+++ javascript/day-1/calorie-counting.js
@@ -0,0 +1,43 @@
+/*
+set maxCal = 0
+set sum = 0
+read file into array
+loop through each array item
+    if item isn't whitespace, add it to sum
+        if maxCal < sum
+            maxCal = sum
+    if item is whitespace
+        sum = 0
+return maxCal
+*/
+const fs = require("fs");
+
+const readFile = (path) => {
+	let data = fs.readFileSync(__dirname + path, "utf-8");
+	data = data
+		.toString()
+		.split("\n")
+		.map((val) => {
+			return val === "" ? "" : parseInt(val);
+		});
+	return data;
+};
+
+const getMaxCals = (data) => {
+	let maxCals = 0;
+	let sum = 0;
+	for (let value of data) {
+		if (value !== "") {
+			sum += value;
+			if (maxCals < sum) {
+				maxCals = sum;
+			}
+		} else {
+			sum = 0;
+		}
+	}
+	return maxCals;
+};
+
+let data = readFile("/input.txt");
+console.log(getMaxCals(data));
