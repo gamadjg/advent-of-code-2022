424f177a838b26b40ad016d25780e2426cf90cea
gamadjg <gamadavid36@gmail.com>
2023-01-03 09:23:29 -0800

    day 6 part 1 & 2 complete

---
+++ javascript/day-6/p1-tuning-trouble.js
@@ -0,0 +1,41 @@
+/*
+Create empty object buffer
+for every character
+    if the buffer size is not length 4
+        if the character is in the buffer
+            while buffer[0] is not equal to current character
+                shift the buffer values
+            
+        else
+            add it in with with the key as the character and the index as the value
+    else
+        return the current stream index - 1
+*/
+
+const fs = require("fs");
+const readFile = (path) => {
+	let data = fs.readFileSync(__dirname + path, "utf-8");
+	return data;
+};
+
+const findBufferEnd = (stream) => {
+	let buffer = [];
+	for (let [index, current] of stream.entries()) {
+		if (buffer.length < 4) {
+			if (buffer.includes(current)) {
+				while (buffer[0] !== current) {
+					buffer.shift();
+				}
+				buffer.shift();
+			}
+			buffer.push(current);
+		} else {
+			return index;
+		}
+	}
+	return "No message found.";
+};
+
+let stream = readFile("/input.txt");
+stream = stream.split("");
+console.log(findBufferEnd(stream));
